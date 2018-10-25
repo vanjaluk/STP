ef19c0d5501923bcff53ce628536c9afdcccf3d2
Vanja Luk <vanja.luk@infobip.com>
2018-10-25 13:56:07 +0200

    zadaca 2a

---
+++ src/hr/unipu/zadace/zad2a/Main.java
@@ -0,0 +1,11 @@
+package hr.unipu.zadace.zad2a;
+
+
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main (String[] args) {
+    }
+}

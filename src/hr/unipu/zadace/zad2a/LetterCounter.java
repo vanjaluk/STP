742f8ad657951b43df8828854243d2bcf3801125
Vanja <vanja.luk@gmail.com>
2018-10-27 23:33:48 +0200

    zadaca 2a

--- src/hr/unipu/zadace/zad2a/LetterCounter.java
+++ src/hr/unipu/zadace/zad2a/LetterCounter.java
@@ -1,43 +1,56 @@
 package hr.unipu.zadace.zad2a;
 
-
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
 
 public class LetterCounter {
     public static void main (String[] args) {
+        System.out.println("Zapiši riječi, nakon svake stisni enter. Kad završiš napiši \"kraj\"");
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> StringLista = new ArrayList<> ();
-
-
+        ArrayList<String> StringList = new ArrayList<String> ();
         String s="";
 
-        while (!s.equals("kraj")) {
-            StringLista.add(sc.nextLine());
-            s = StringLista.get(StringLista.size()-1);
+        while (!s.equalsIgnoreCase("kraj")) {
+            StringList.add(sc.nextLine());
+            s = StringList.get(StringList.size()-1);
         }
-        System.out.println("riječi s jednakim brojem...");
 
-        for (int i=0; i < StringLista.size(); i++) {
-            String z = StringLista.get(i);
-            int sam = 0;
-            int sug = 0;
-            for ( int k=0; k < z.length(); k++) {
-                char x = z.charAt(k);
+        if (StringList.size()-1 == 0) {
+            System.out.println("Nisi ubacio ni jednu riječ.");
+        }
 
-                if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
-                    sam++;
-                } else {
-                    sug++;
-                }
-            if (sam == sug ){System.out.println(z);}
+        else {
+            System.out.println("\nUpisane su ove riječi: ");
+            int words = 0;
+            for (int i=0; i<StringList.size()-1;i++) {
+                System.out.print(StringList.get(i).toUpperCase()+"  ");
+                ++words;
             }
+            System.out.println("\nBroj upisanih riječi: " + words);
 
+            System.out.println("\nOvo su riječi koje imaju isti broj suglasnika i samoglasnika:\n\n" +
+                               "|  br | sam | sug | riječ\n+-----+-----+-----+--------");
+            for (int i = 0; i < StringList.size(); i++) {
+                ++words;
+                String z = StringList.get(i);
+
+                int a = 0;
+                int b = 0;
+
+                for (int k = 0; k < z.length(); k++) {
+                    char x = z.charAt(k);
+
+                    if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+                        a++;
+                    } else {
+                        b++;
+                    }
+                }
+                if (a == b) {
+                    System.out.println("|  " + z.length() + "  |  " + a + "  |  " + b + "  | " + z.toUpperCase());
+                    }
+                }
+            }
         }
-
-        }
-
     }

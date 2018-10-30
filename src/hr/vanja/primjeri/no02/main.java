b4bc3d1d699f3600426a00fe1e92210f66c54cef
Vanja <vanja.luk@gmail.com>
2018-10-30 19:46:26 +0100

    example 2

--- src/hr/vanja/primjeri/no02/main.java
+++ src/hr/vanja/primjeri/no02/main.java
@@ -1,4 +1,35 @@
 package hr.vanja.primjeri.no02;
 
+import org.jetbrains.annotations.Contract;
+
 public class main {
+    public static void main(String[] args) {
+
+        int highScorePostion = calculateHighScorePosition(1500);
+        dsiplayHighScorePosition("Mile", highScorePostion);
+
+        highScorePostion = calculateHighScorePosition(900);
+        dsiplayHighScorePosition("Slavko", highScorePostion);
+
+        highScorePostion = calculateHighScorePosition(400);
+        dsiplayHighScorePosition("Branko", highScorePostion);
+
+        highScorePostion = calculateHighScorePosition(50);
+        dsiplayHighScorePosition("Drago", highScorePostion);
+
+    }
+
+    public static void dsiplayHighScorePosition (String playerName, int highScorePosition) {
+        System.out.println(playerName + " stigao je do pozicije: " + highScorePosition);
+        }
+
+
+    public static int calculateHighScorePosition (int playerScore) {
+        if(playerScore>1000) {return 1;}
+        else if(playerScore>500 && playerScore<1000) {return 2;}
+        else if(playerScore>100 && playerScore<500) {return 2;}
+        else return 4;
+        }
+
 }
+

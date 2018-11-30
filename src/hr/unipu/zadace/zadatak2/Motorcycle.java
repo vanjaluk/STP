b53b9b2239ecffcd58688dcbe851099f541d4c8b
Vanja <vanja.luk@gmail.com>
2018-11-30 22:48:33 +0100

    update

--- src/hr/unipu/zadace/zadatak2/Motorcycle.java
+++ src/hr/unipu/zadace/zadatak2/Motorcycle.java
@@ -1,5 +1,7 @@
 package hr.unipu.zadace.zadatak2;
 
 public class Motorcycle extends Veichle {
-
+    public Motorcycle(String manufacturer, String model, String id, String power, String numberOfPassengers, double regularPrice) {
+        super(manufacturer, model, id, power, numberOfPassengers, regularPrice);
+    }
 }

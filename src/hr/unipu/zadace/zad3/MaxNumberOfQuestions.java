9d35983c6f5552239c82e4f63934fd9d2cf950bc
Vanja Luk <vanja.luk@infobip.com>
2019-01-14 13:28:33 +0100

    zadaca 6

---
+++ src/hr/unipu/zadace/zad3/MaxNumberOfQuestions.java
@@ -0,0 +1,16 @@
+package hr.unipu.zadace.zad3;
+
+public class MaxNumberOfQuestions extends Exception {
+
+
+    public MaxNumberOfQuestions() {
+    }
+
+    public MaxNumberOfQuestions(String message) {
+        super(message);
+    }
+
+    public MaxNumberOfQuestions(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
